ba9cfb48e98fb1c4cfbe94ea4ad7ee44ab7f15b4
Gilangsymdr <gilanggalang055@gmail.com>
2023-03-12 18:47:48 +0700

    edit

--- src/component/Header/UI/Footer.jsx
+++ src/component/Header/UI/Footer.jsx
@@ -8,59 +8,59 @@ export default function Footer () {
     <div className="footer">
       <div className="container">
         <div className="row">
-          <div className="col-auto">
-          <div className="logo">
+          <div className="col-4">
+          <div className="logo-footer">
         <a><span>Stay</span>cation</a>
           </div>
           <p className="brand-tagline">
             We kaboom your beauty holiday instantly and memorable
           </p>
           </div>
-          <div className="col-2">
+          <div className="col-3">
             <h6 className="mt-2">For Beginners</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <Button type='link' href='/register'>New Account Register</Button>
+                <Button className='list-item' type='link' href='/register' >New Account Register</Button>
               </li>
               <li className="list-group-item">
-                <Button type='link' href='/properties'>Start Booking Room</Button>
+                <Button type='link' href='/properties' className='list-item' >Start Booking Room</Button>
               </li>
               <li className="list-group-item">
-                <Button type='link' href='/use-payment'>Use Payment</Button>
+                <Button type='link' href='/use-payment' className='list-item' >Use Payment</Button>
               </li>
             </ul>
           </div>
-          <div className="col-2 mr-2">
+          <div className="col-3 mr-2">
             <h6 className="mt-2">Explore Us</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <Button type='link' href='/career'>Our Careers</Button>
+                <Button className='list-item' type='link' href='/career'>Our Careers</Button>
               </li>
               <li className="list-group-item">
-                <Button type='link' href='/privacy'>Privacy</Button>
+                <Button className='list-item' type='link' href='/privacy'>Privacy</Button>
               </li>
               <li className="list-group-item">
-                <Button type='link' href='/terms'>term Conditions</Button>
+                <Button className='list-item' type='link' href='/terms'>term Conditions</Button>
               </li>
             </ul>
           </div>
-          <div className="col-4">
+          <div className="col-2">
           <h6 className="mt-2">Connect Us</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <Button isExternal type='link' href='mailto:staycation@gamil.com'>supportstaycation.id</Button>
+                <Button isExternal className='list-item' type='link' href='mailto:staycation@gamil.com'>supportstaycation.id</Button>
               </li>
               <li className="list-group-item">
-                <Button isExternal type='link' href='tel:+62081342536'>0854 - 5643 - 8765</Button>
+                <Button isExternal className='list-item' type='link' href='tel:+62081342536'>0854 - 5643 - 8765</Button>
               </li>
-              <li className="list-group-item">
+              <li className="list-group-item" style={{margimBottom: 20}}>
                 <span className='addres'>Jombang, Ploso, Jawa Timur</span>
               </li>
             </ul>
           </div>
         </div>
         <div className="row">
-          <div className="col text-center copyrights">
+          <div className="col text-center copyrights" style={{paddingTop: 70}}>
             Copyrights 2019 . All right reserved . Staycation
           </div>
         </div>
