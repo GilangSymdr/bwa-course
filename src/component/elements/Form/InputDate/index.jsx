fc629fc90ae05cdf85758b2f9802cbbed1a16b47
Gilangsymdr <gilanggalang055@gmail.com>
2023-03-14 18:16:09 +0700

    add InputNumber

--- src/component/elements/Form/InputDate/index.jsx
+++ src/component/elements/Form/InputDate/index.jsx
@@ -1,56 +1,49 @@
-import React, { useState } from 'react';
-import propTypes from 'prop-types';
-import '../../Form/InputDate/index.test';
+import React, { useState } from "react";
+import propTypes from "prop-types";
+import "./index.scss";
 
-export default function Number (props) {
+export default function Number(props) {
+  const { value, placeholder, name, min, max, prefix, suffix } = props;
 
-  const { value, placeholder, name, min, max, suffix, prefix} = props;
+  const [InputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
 
-  const [ InputValue, setInputValue ] = useState(`${prefix}${value}${suffix}`); 
-
-  const onChange = e => {
-    Let.value = String(e.target.value);
-    if (prefix) value  = value.replace(prefix);
-    if (suffix) value  = value.replace(suffix);
+  const onChange = (e) => {
+    let value = String(e.target.value);
+    if (prefix) value = value.replace(prefix);
+    if (suffix) value = value.replace(suffix);
 
     const patternNumeric = new RegExp("[0-9]*");
     const isNumeric = patternNumeric.test(value);
 
-    if ( Numeric && +value <= max && +value >= min) {
-
-      props.onChange ({
-
+    if (isNumeric && +value <= max && +value >= min) {
+      props.onChange({
         target: {
           name: name,
-          value: +value
-        }
+          value: +value,
+        },
       });
       setInputValue(`${prefix}${value}${suffix}`);
     }
   };
 
   const minus = () => {
-
     value > min &&
-    onChange({
-      target: {
-        name: name,
-        value: +value - 1
-      }
-    });
+      onChange({
+        target: {
+          name: name,
+          value: +value - 1,
+        },
+      });
   };
-
   const plus = () => {
-
-    value < max && 
-    onChange({
-      target: {
-        name: name,
-        value: +value + 1
-      }
-    });
+    value < max &&
+      onChange({
+        target: {
+          name: name,
+          value: +value + 1,
+        },
+      });
   };
-
   return (
     <div className={["input-number mb-3", props.outerClassName].join(" ")}>
       <div className="input-group">
@@ -60,14 +53,14 @@ export default function Number (props) {
           </span>
         </div>
         <input
-        min={min}
-        max={max}
-        name={name}
-        pattern="[0-9]*"
-        className='form-control'
-        placeholder={ placeholder ? placeholder : "0"}
-        value={String(InputValue)}
-        onChange={onChange}
+          min={min}
+          max={max}
+          name={name}
+          pattern="[0-9]*"
+          className="form-control"
+          placeholder={placeholder ? placeholder : "0"}
+          value={String(InputValue)}
+          onChange={onChange}
         />
         <div className="input-group-append">
           <span className="input-group-text plus" onClick={plus}>
@@ -76,19 +69,19 @@ export default function Number (props) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 Number.defaultProps = {
   min: 1,
   max: 1,
   prefix: "",
-  suffix: ""
-}
+  suffix: "",
+};
 
 Number.propTypes = {
-  value: propTypes.oneOfType([propTypes.string, propTypes.Number]),
+  value: propTypes.oneOfType([propTypes.string, propTypes.number]),
   onChange: propTypes.func,
   placeholder: propTypes.string,
-  outerClassName: propTypes.string
-}
+  outerClassName: propTypes.string,
+};
